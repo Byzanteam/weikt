b524e1b35d63c9facf1a8ec3aab91a74f509658f
peater <621601RQY>
2018-10-31 18:43:46 +0800

    sql

--- public/sql.php
+++ public/sql.php
@@ -10,12 +10,13 @@
 $_sql = file_get_contents('weikt_webuildus.sql');
 
 
-print_r(getenv());
 $_arr = explode(';', $_sql);
 $_mysqli = new mysqli(getenv('DATABASE_URL'), getenv('DOKKU_MYSQL_WEIKT_DB_ENV_MYSQL_USER'), getenv('DOKKU_MYSQL_WEIKT_DB_ENV_MYSQL_ROOT_PASSWORD'));
 if (mysqli_connect_errno()) {
     exit('连接数据库出错');
 }
+
+$_mysqli->query('USE ' . getenv('DOKKU_MYSQL_WEIKT_DB_ENV_MYSQL_DATABASE'));
 //执行sql语句
 foreach ($_arr as $_value) {
     $_mysqli->query($_value.';');
