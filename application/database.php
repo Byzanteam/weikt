51ae2bf6424a4fefdb278fa5291acddb73a85fba
peater <621601RQY>
2018-10-31 14:42:16 +0800

    课件数据文件

--- application/database.php
+++ application/database.php
@@ -13,7 +13,7 @@ return [
     // 数据库类型
     'type'            => getenv('DB_CONNECTION'),
     // 服务器地址
-    'hostname'        => getenv('DB_HOST'),
+    'hostname'        => getenv('DATABASE_URL'),
     // 数据库名
     'database'        => getenv('DB_DATABASE'),
     // 用户名
