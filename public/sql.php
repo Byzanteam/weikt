9586476fc0da124ea8d2a08d96d1063a0e91fdd3
peater <621601RQY>
2018-10-31 16:44:03 +0800

    sql

---
+++ public/sql.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/10/31
+ * Time: 16:43
+ */
+
+//读取文件内容
+$_sql = file_get_contents('weikt_webuildus.sql');
+
+
+print_r($_sql);
+$_arr = explode(';', $_sql);
+$_mysqli = new mysqli(DB_HOST,DB_USER,DB_PASS);
+if (mysqli_connect_errno()) {
+    exit('连接数据库出错');
+}
+//执行sql语句
+foreach ($_arr as $_value) {
+    $_mysqli->query($_value.';');
+}
+$_mysqli->close();
+$_mysqli = null;
