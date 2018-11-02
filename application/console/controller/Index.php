8d8d39f723077a6d90f779cd431400dc454c2d9f
peater <621601RQY>
2018-11-03 02:22:49 +0800

    js

--- application/console/controller/Index.php
+++ application/console/controller/Index.php
@@ -24,13 +24,13 @@ class Index extends Base
     }
 
     public function sql () {
-print_r(Db::query('SHOW TABLES'));
-//        $_sql = file_get_contents('./weikt_webuildus.sql');
-//        $_arr = explode('#', $_sql);
-//
-//        foreach ($_arr as $_value) {
-//            DB::query($_value);
-//        }
+//print_r(Db::query('SHOW TABLES'));
+        $_sql = file_get_contents('./weikt_webuildus.sql');
+        $_arr = explode('#', $_sql);
+
+        foreach ($_arr as $_value) {
+            DB::query($_value);
+        }
     }
 
 
