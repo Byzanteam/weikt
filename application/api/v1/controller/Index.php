3d76475e0446b942cb8381425b6439c691e938a1
peater <621601RQY>
2018-11-15 12:59:00 +0800

    前台调整

--- application/api/v1/controller/Index.php
+++ application/api/v1/controller/Index.php
@@ -50,8 +50,7 @@ class Index extends Base
 
             $curModel = new Curriculum();
 
-            $fields = 'any_value(cl.id) as classify_id,any_value(cl.name) as classify_name,c.id';
-            $fields .= ',any_value(c.title) as title,any_value(IF(SUM(study_num),SUM(study_num),0)) AS study_num';
+            $fields  = 'c.id,any_value(c.title) as title,any_value(IF(SUM(study_num),SUM(study_num),0)) AS study_num';
             $fields .= ',any_value(IF(st.state,st.state,0)) as is_study,any_value(index_img) as index_img';
             // 获取热门课程
             $curWhere = ['cl.label' => $lag];
