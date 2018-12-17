fb85ddfc7df8950349f0cae32cfcbdecd6a7a3ce
peater <621601RQY>
2018-12-17 11:48:51 +0800

    样式更新

--- application/console/controller/Index.php
+++ application/console/controller/Index.php
@@ -23,30 +23,4 @@ class Index extends Base
         return $this->fetch('/console/index/main');
     }
 
-    public function sql () {
-
-        DB::query('DELETE FROM vcr_user_basic WHERE nickname=\'4oqZX+KKmQ==\' AND `name`=\'⊙_⊙\'');
-//        Db::query('ALTER TABLE `vcr_curriculum` MODIFY COLUMN `title`  varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT \'课程标题\' AFTER `id`');
-        print_r(1111);
-//        foreach ($user as $k=>$v) {
-//
-//            if($v['nickname'] != base64_encode(base64_decode($v['nickname'])) || $v['nickname'] == 'What') {
-////                echo $v['nickname'].PHP_EOL;
-//
-//                $sql = 'UPDATE vcr_user_basic SET nickname=\'' . base64_encode($v['nickname']) . '\'';
-//                $sql .= ' WHERE id=' . $v['id'];
-//
-//                DB::query($sql);
-//
-//            }
-//        }
-
-//        $_sql = file_get_contents('./weikt_webuildus.sql');
-//        $_arr = explode('#', $_sql);
-//
-//        foreach ($_arr as $_value) {
-//            DB::query($_value);
-//        }
-    }
-
 }
